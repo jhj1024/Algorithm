5ed063df5416b9fb60fcd21af74af3ef844dc296
jhj1024 <jhjung1402@gmail.com>
2020-08-05 17:56:30 +0900

    0805

---
+++ BOJ/BOJ_S1_2667_단지번호붙이기_BFS.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+/**
+* @author JUNG
+* @name BOJ_S1_2667_단지번호붙이기
+* @date 2020.08.05
+* @link https://www.acmicpc.net/problem/2667
+* @mem
+* @time
+* @caution 
+* [고려사항] 대각선상에 집이 있는 경우는 연결된 것이 아니다. (사방연결만 가능)
+* [입력사항]
+* [출력사항]
+*/
+
+public class BOJ_S1_2667_단지번호붙이기_BFS {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder output = new StringBuilder();
+    static String line;
+    static int cnt, N, Answer=0;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static List<Integer> houseCnt = new ArrayList<Integer>();
+    
+    public static void main(String[] args) throws Exception{
+        input = new BufferedReader(new StringReader(src));
+        
+        //입력
+        N = Integer.parseInt(input.readLine());
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        
+        for(int i = 0; i < N; i++) {
+            line = input.readLine();
+            for(int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0'; //char에서 int로 변환
+            }
+        }        
+
+        /* -------------------- 알고리즘  -------------------- */
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(map[i][j] == 1) {
+                    cnt = 1;
+                    map[i][j] = -1; //방문표시
+                    bfs(new pair(i, j));
+                    Answer++; //단지 수 증가
+                    houseCnt.add(cnt); //단지 내 집의 개수 추가
+                }
+            }
+        } 
+        
+        Collections.sort(houseCnt); //집의 개수는 오름차순 정렬
+        
+        output.append(Answer).append("\n");
+        for(Integer data : houseCnt) {
+            output.append(data).append("\n");
+        }
+        System.out.print(output);       
+    }
+    
+    static void bfs(pair start) {
+        Queue<pair> queue = new LinkedList<pair>();
+        queue.offer(start);
+        
+        while(!queue.isEmpty()) {
+            pair p = queue.poll();
+            int r = p.i; int c = p.j;
+            
+            for(int d = 0; d < dirs.length; d++) {
+                int dr = r + dirs[d][0];
+                int dc = c + dirs[d][1];
+                
+                if(0 <= dr && dr < N && 0 <= dc && dc < N && map[dr][dc] == 1) { //연결되어있으면
+                    map[dr][dc] = -1; //방문표시
+                    cnt++;
+                    queue.offer(new pair(dr, dc));
+                }
+            }
+        }
+    }
+    
+    static class pair{
+        int i, j;
+
+        public pair() { }
+        
+        public pair(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+    }
+    
+    
+    static String src = "7\r\n" + 
+            "0110100\r\n" + 
+            "0110101\r\n" + 
+            "1110101\r\n" + 
+            "0000111\r\n" + 
+            "0100000\r\n" + 
+            "0111110\r\n" + 
+            "0111000";
+
+}
