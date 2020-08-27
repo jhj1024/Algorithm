fd272708f010b1ce8e8029d09748afbb19315430
JUNG <jhjung1402@gmail.com>
2020-08-27 15:52:10 +0900

    0826

--- BOJ/BOJ_S3_2615_오목.java
+++ BOJ/BOJ_S3_2615_오목.java
@@ -13,13 +13,12 @@ import java.util.StringTokenizer;
 * @mem
 * @time
 * @caution
-* [고려사항] (1,1)이 시작임
+* [고려사항] (1,1)이 시작임, 육목이면 해당 바둑알들은 정답 처리 안함
 * [입력사항] 19*19 크기의 바둑판 정보
 * [출력사항] 첫줄에 검은색이 이겼을 경우에는 1을, 흰색이 이겼을 경우에는 2를, 아직 승부가 결정되지 않았을 경우에는 0을 출력, 둘째 줄에 연속된 다섯 개의 바둑알 중 가장 처음에 위치한 바둑알의 좌표
 * 
 * 맵에 일단 저장 후, 검은색, 흰색 좌표는 각각의 어레이에 저장
 * 가장 처음에 위치한 거를 찾는 거니까 (오른쪽 탐색, 아래 탐색, 오른아래 탐색) 세가지로 확인 -> 5번째 끝에 있는게 다른 색이면 탐색할 가치가 없음
-* 크기가 3인 배열을 만들어서 중간에 막히면 -1을 넣고, 더이상 그부분 탐색 안함
 */
 
 public class BOJ_S3_2615_오목 {
@@ -78,7 +77,7 @@ public class BOJ_S3_2615_오목 {
                 continue;
             }
 
-            for(int i = 1; i <= 5; i++) { //출발점을 뺀 4개가 다 같은 색인지 확인하고
+            for(int i = 1; i <= 5; i++) { //출발점을 뺀 4개가 다 같은 색인지 확인하고 -> 육목 방지를 위해 임의로 한번 더 체크
                 dr = start.r + dirs[d][0]*i;
                 dc = start.c + dirs[d][1]*i;
                 if(!isIn(dr, dc ) || color != map[dr][dc]) {
@@ -88,11 +87,12 @@ public class BOJ_S3_2615_오목 {
             }
         }
 
+        //오목이면(육목은 5가 저장됨)
         if(count[0] == 4 || count[1] == 4 || count[2] == 4 || count[3] == 4) {
             Answer = color;
             r = start.r;
             c = start.c;
-            return true;
+            return true; //정답 찾음
         }
         return false;
     }
@@ -100,8 +100,7 @@ public class BOJ_S3_2615_오목 {
     static boolean isIn(int r, int c) {
         return (0 < r && r < R && 0 < c && c < C); //범위 안에 있는지 확인(1부터 시작)
     }
-    
-    
+
     static class Stone implements Comparable<Stone>{
         int r, c;
 
@@ -110,25 +109,6 @@ public class BOJ_S3_2615_오목 {
             this.r = r;
             this.c = c;
         }
-
-        @Override
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            builder.append("(");
-            builder.append(r);
-            builder.append(", ");
-            builder.append(c);
-            builder.append(")");
-            return builder.toString();
-        }
-
-        @Override
-        public int compareTo(Stone o) { //열 오름차순
-            if(this.r == o.r) {
-                return Integer.compare(this.r, o.r);
-            }
-            return Integer.compare(this.c, o.c);
-        }   
     }
     
     static String src = 
